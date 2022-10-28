72f9c0200d03104ba4a6ae30f683e9a81412882e
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-10-28 14:24:19 +0530

    learn Getter and setter Methods

---
+++ BikeRider.java
@@ -0,0 +1,13 @@
+class BikeRider
+{
+public static void main(String[] args)
+{
+PetrolBunk hp = new PetrolBunk(); 
+
+
+
+hp.setPetrol_Price(102.8f);
+
+}
+
+}
---
+++ PetrolBunk.java
@@ -0,0 +1,16 @@
+class PetrolBunk
+{
+private float petrol_price = 103.8f; 
+
+float getPetrol_price()
+{ // getter method. 
+return this.petrol_price; 
+}
+
+void setPetrol_Price(float petrol_price)
+{
+this.petrol_price = petrol_price; 
+}
+
+
+}
